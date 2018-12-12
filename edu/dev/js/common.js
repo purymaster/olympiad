3554a9cb7fc4e03f2a1e84a0465e5713a652c67f
RING <purymaster@naver.com>
2018-12-12 17:32:01 +0900

    저장소 재설정

---
+++ edu/dev/js/common.js
@@ -0,0 +1,82 @@
+$(function() {
+
+    var screen = $('html,body'),
+        screen_fix = 0;
+
+    /******************** 네비게이션 제어 ********************/
+
+    var nav_open_btn = $('.header_wrap .menu'),
+        nav_close_btn = $('nav .close'),
+        nav = $('nav');
+
+    //네비게이션 열기
+    nav_open_btn.on('click', function() {
+        screen.addClass('fixed');
+        nav.addClass('on');
+        screen_fix = 1;
+    });
+
+    //네비게이션 닫기
+    nav_close_btn.on('click', function() {
+        screen.removeClass('fixed');
+        nav.removeClass('on');
+        screen_fix = 0;
+    });
+
+    //네비게이션, 팝업 오픈시 스크롤 방지
+    screen.on('scroll touchmove', function(e) {
+        if (screen_fix)
+            return false;
+    });
+
+    /******************** 애니메이션 정의 ********************/
+
+    var move_el = $('*[data-animation]'), //무빙 요소
+        move_name, //무빙 정의
+        move_delay, //순차무빙 딜레이
+        move_duration, //순차무빙 시간
+        scroll, //스크롤 값
+        start_point = $(window).height() - 50, //애니메이션 시작 높이(밑에서부터 50px)
+        top_btn = $('.move_top'), //TOP 버튼
+        top_btn_flag = 0; //TOP 버튼 상태
+
+    move_el.addClass('wait-animation');
+    $(window).on('load scroll', function() {
+        scroll = $(this).scrollTop();
+
+        //순차 애니메이션 제어
+        move_el.each(function() {
+            move_name = $(this).data('animation');
+            move_delay = $(this).data('delay') * 100; //단위 0.1초
+            move_duration = $(this).data('duration') * 1000; //단위 1초
+            $(this).addClass('animated ' + move_name);
+            if (move_delay >= 0)
+                $(this).css({
+                    '-webkit-animation-delay': move_delay + 'ms',
+                    'animation-delay': move_delay + 'ms'
+                });
+            if (move_duration >= 0)
+                $(this).css({
+                    '-webkit-animation-duration': move_duration + 'ms',
+                    'animation-duration': move_duration + 'ms'
+                });
+            if (scroll > $(this).offset().top - start_point)
+                $(this).removeClass('wait-animation');
+        });
+
+        //TOP 버튼 제어
+        scroll > $(window).height() / 2 ? //TOP 버튼 출현 시점(반 페이지 이상 스크롤)
+            top_btn.addClass('on') :
+            top_btn.removeClass('on');
+        top_btn.find('a').on('click', function() {
+            if (top_btn_flag) return false;
+            top_btn_flag = 1;
+            $('html,body').animate({
+                scrollTop: 0
+            }, 500, function() {
+                top_btn_flag = 0;
+            });
+            return false;
+        });
+    });
+});
---
+++ map_asset/map.js
@@ -0,0 +1,45 @@
+$(function () {
+
+    //테이블 제어
+    var table = $("table.list");
+    table.each(function () {
+        $(this).find("tr:gt(0)").hover(function () {
+            $(this).addClass("hover");
+        }, function () {
+            $(this).removeClass("hover");
+        }).each(function (e) {
+            $("td:eq(0)", this).text(e + 1);
+            if ($("td:eq(1)", this).text() != "") $(this).addClass("part");
+        }).find("td").each(function () {
+            if ($(this).text() == "") $(this).addClass("nodata");
+        });
+    });
+
+    //작업공정률
+    $('.map_wrap').each(function () {
+        $(this).find(".process dd").text(Math.ceil(($(this).find("em.end").length - 1) / $(this).find("tbody").children("tr").length * 100) + "%");
+    });
+
+    //iframe 내용 및 슬라이드 제어
+    var source;
+    $('#canvas').attr('src', $('.map_wrap:eq(0) table tbody tr:eq(0)').find('a').attr('href'));
+    $('table tbody tr').each(function () {
+        source = $(this).find('a').attr('href');
+        $(this).attr('data-src', source);
+    }).on('click', function () {
+        $('#canvas').attr('src', $(this).attr('data-src'));
+        $('.slide_wrap').removeClass('on');
+    });
+    $('.slide_wrap .map').on('click', function () {
+        $('.slide_wrap').addClass('on');
+    });
+
+    //iframe 해상도 제어
+    $('.view li:eq(0) button').addClass('on');
+    $('.view').find('button').on('click', function () {
+        $('.view button').removeClass('on');
+        $(this).addClass('on');
+        $('#canvas').removeClass().addClass($(this).attr('data-view'));
+    });
+    
+});
