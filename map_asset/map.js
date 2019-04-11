8f89ca8324f9a685c46d95c4c7d35b83217f9acd
RING <purymaster@naver.com>
2019-04-11 10:35:55 +0900

    코딩맵 수정

--- map_asset/map.js
+++ map_asset/map.js
@@ -19,27 +19,4 @@ $(function () {
     $('.map_wrap').each(function () {
         $(this).find(".process dd").text(Math.ceil(($(this).find("em.end").length - 1) / $(this).find("tbody").children("tr").length * 100) + "%");
     });
-
-    //iframe 내용 및 슬라이드 제어
-    // var source;
-    // $('#canvas').attr('src', $('.map_wrap:eq(0) table tbody tr:eq(0)').find('a').attr('href'));
-    // $('table tbody tr').each(function () {
-    //     source = $(this).find('a').attr('href');
-    //     $(this).attr('data-src', source);
-    // }).on('click', function () {
-    //     $('#canvas').attr('src', $(this).attr('data-src'));
-    //     $('.slide_wrap').removeClass('on');
-    // });
-    // $('.slide_wrap .map').on('click', function () {
-    //     $('.slide_wrap').addClass('on');
-    // });
-
-    //iframe 해상도 제어
-    // $('.view li:eq(0) button').addClass('on');
-    // $('.view').find('button').on('click', function () {
-    //     $('.view button').removeClass('on');
-    //     $(this).addClass('on');
-    //     $('#canvas').removeClass().addClass($(this).attr('data-view'));
-    // });
-    
 });
